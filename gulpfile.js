6df8e787d9b130595f2764a9945a4bb626bb95df
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2016-11-25 22:06:04 +0100

    core: add release tasks

--- gulpfile.js
+++ gulpfile.js
@@ -22,12 +22,17 @@
  * SOFTWARE.
  */
 
+const fs = require('fs');
 const path = require('path');
 const gulp = require('gulp');
 const jasmine = require('gulp-jasmine');
 const babel = require('gulp-babel');
 const del = require('del');
 const eslint = require('gulp-eslint');
+const gutil = require('gulp-util');
+const git = require('gulp-git');
+const bump = require('gulp-bump');
+const runSequence = require('run-sequence');
 
 const root = path.join(__dirname);
 const config = {
@@ -66,4 +71,38 @@ gulp.task('lint', () => {
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
+});
+
+gulp.task('commit:pre', () => {
+  const dist = path.join(__dirname, 'dist');
+  const packageJson = path.join(__dirname, 'package.json');
+  return gulp.src([packageJson, dist])
+    .pipe(git.add({args: '-f'}))
+    .pipe(git.commit('release: release version'));
+});
+
+gulp.task('commit:post', () => {
+  const dist = path.join(__dirname, 'dist');
+  return gulp.src(dist)
+    .pipe(git.rm({args: '-r'}))
+    .pipe(git.commit('release: prepare next release'));
+});
+
+gulp.task('tag', (done) => {
+  const pkg = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8');
+  const version = JSON.parse(pkg).version;
+  git.tag(`v${version}`, `release: tag version ${version}`, done);
+});
+
+['major', 'minor', 'patch'].forEach((level) => {
+  gulp.task(`bump:${level}`, () => {
+    return gulp.src(path.join(__dirname, 'package.json'))
+      .pipe(bump({type: level})
+      .on('error', gutil.log))
+      .pipe(gulp.dest(__dirname));
+  });
+
+  gulp.task('release:' + level, ['build'], () => {
+    return runSequence(`bump:${level}`, 'commit:pre', 'tag', 'commit:post');
+  });
 });
