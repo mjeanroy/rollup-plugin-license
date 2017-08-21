92945650a30134a63c974264a82255b143d5594f
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2017-08-21 21:58:43 +0200

    chore: add CHANGELOG and add a task to re-generate it

--- gulpfile.js
+++ gulpfile.js
@@ -30,76 +30,81 @@ const babel = require('gulp-babel');
 const del = require('del');
 const eslint = require('gulp-eslint');
 const gutil = require('gulp-util');
+const conventionalChangelog = require('gulp-conventional-changelog');
 const git = require('gulp-git');
 const bump = require('gulp-bump');
 const runSequence = require('run-sequence');
 
-const root = path.join(__dirname);
-const config = {
-  src: path.join(root, 'src'),
-  test: path.join(root, 'test'),
-  dist: path.join(root, 'dist'),
-};
+const ROOT = __dirname;
+const SRC = path.join(ROOT, 'src');
+const TEST = path.join(ROOT, 'test');
+const DIST = path.join(ROOT, 'dist');
 
 gulp.task('test', ['build'], () => {
   const src = [
-    path.join(config.test, 'base.spec.js'),
-    path.join(config.test, '**', '*.spec.js'),
+    path.join(TEST, 'base.spec.js'),
+    path.join(TEST, '**', '*.spec.js'),
   ];
 
-  return gulp
-    .src(src)
-    .pipe(jasmine());
+  return gulp.src(src).pipe(jasmine());
 });
 
 gulp.task('clean', () => {
-  return del([config.dist]);
+  return del([DIST]);
 });
 
 gulp.task('build', ['clean', 'lint'], () => {
-  return gulp
-    .src(path.join(config.src, '*.js'))
+  return gulp.src(path.join(SRC, '*.js'))
     .pipe(babel())
-    .pipe(gulp.dest(path.join(__dirname, 'dist')));
+    .pipe(gulp.dest(DIST));
 });
 
 gulp.task('lint', () => {
-  const srcFiles = path.join(config.src, '**', '*.js');
-  const testFiles = path.join(config.test, '**', '*.js');
-  return gulp
-        .src([srcFiles, testFiles])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+  const src = [
+    path.join(SRC, '**', '*.js'),
+    path.join(TEST, '**', '*.js'),
+  ];
+
+  return gulp.src(src)
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('changelog', () => {
+  const changelog = path.join(ROOT, 'CHANGELOG.md');
+  return gulp.src(changelog, {buffer: false})
+    .pipe(conventionalChangelog({
+      releaseCount: 0,
+    }))
+    .pipe(gulp.dest(ROOT));
 });
 
 gulp.task('commit:pre', () => {
-  const dist = path.join(__dirname, 'dist');
-  const packageJson = path.join(__dirname, 'package.json');
-  return gulp.src([packageJson, dist])
+  const packageJson = path.join(ROOT, 'package.json');
+  return gulp.src([packageJson, DIST])
     .pipe(git.add({args: '-f'}))
     .pipe(git.commit('release: release version'));
 });
 
 gulp.task('commit:post', () => {
-  const dist = path.join(__dirname, 'dist');
-  return gulp.src(dist)
+  return gulp.src(DIST)
     .pipe(git.rm({args: '-r'}))
     .pipe(git.commit('release: prepare next release'));
 });
 
 gulp.task('tag', (done) => {
-  const pkg = fs.readFileSync(path.join(__dirname, 'package.json'), 'utf8');
-  const version = JSON.parse(pkg).version;
+  const pkg = require(path.join(ROOT, 'package.json'));
+  const version = pkg.version;
   git.tag(`v${version}`, `release: tag version ${version}`, done);
 });
 
 ['major', 'minor', 'patch'].forEach((level) => {
   gulp.task(`bump:${level}`, () => {
-    return gulp.src(path.join(__dirname, 'package.json'))
+    return gulp.src(path.join(ROOT, 'package.json'))
       .pipe(bump({type: level})
       .on('error', gutil.log))
-      .pipe(gulp.dest(__dirname));
+      .pipe(gulp.dest(ROOT));
   });
 
   gulp.task('release:' + level, ['build'], () => {
