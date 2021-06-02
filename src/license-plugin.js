0bc7d809952fa9a7803d51fa3c1ee41900250a14
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2021-06-02 15:13:31 +0200

    chore: fix glob case insensitivity on windows

--- src/license-plugin.js
+++ src/license-plugin.js
@@ -186,7 +186,7 @@ class LicensePlugin {
           pkg = pkgJson;
 
           // Read license file, if it exists.
-          const licenseFile = glob.sync(path.join(dir, 'LICENSE*'), {nocase: true})[0];
+          const licenseFile = glob.sync(path.join(dir, 'LICENSE*'), {nocase: true, strict: false, silent: true})[0];
           if (licenseFile) {
             pkg.licenseText = fs.readFileSync(licenseFile, 'utf-8');
           }
