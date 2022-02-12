2ac4b2421052dc57f6ef15b962a54d088c6b9103
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2022-02-12 23:42:40 +0100

    chore: migrate to jasmine 4, remove gulp-jasmine

--- scripts/test/index.js
+++ scripts/test/index.js
@@ -24,7 +24,7 @@
 
 const path = require('path');
 const gulp = require('gulp');
-const jasmine = require('gulp-jasmine');
+const Jasmine = require('jasmine');
 const config = require('../config');
 
 /**
@@ -33,12 +33,11 @@ const config = require('../config');
  * @return {WritableStream} The gulp pipe stream.
  */
 function test() {
-  const src = [
-    path.join(config.test, 'base.spec.js'),
-    path.join(config.test, '**', '*.spec.js'),
-  ];
-
-  return gulp.src(src).pipe(jasmine());
+  return createJasmine().execute().then((result) => {
+    if (result.overallStatus !== 'passed') {
+      throw new Error('Failing tests');
+    }
+  });
 }
 
 /**
@@ -48,10 +47,77 @@ function test() {
  * @return {void}
  */
 function tdd(done) {
+  const test = () => (
+    createJasmine().execute()
+  );
+
   gulp.watch(path.join(config.src, '**', '*.js'), test);
   gulp.watch(path.join(config.test, '**', '*.js'), test);
-
   done();
+}
+
+/**
+ * Initialize Jasmine instance.
+ *
+ * @returns {Object} The jasmine instance.
+ */
+function createJasmine() {
+  // Override the default loader to be sure it loads file using cache invalidation (useful for hot-reload).
+  // jasmine/lib/loader.js is not exported, nor is in the `"exports`" field of `jasmine/package.json`
+  // The only way is load it using an absolute path...
+  const jasmineDir = path.dirname(require.resolve('jasmine'));
+  const Loader = require(path.join(jasmineDir, 'loader.js'));
+  const jasmine = new Jasmine({
+    loader: new Loader({
+      requireShim: requireNoCache,
+    }),
+  });
+
+  jasmine.exitOnCompletion = false;
+
+  jasmine.loadConfig({
+    // Without this config, the default loader will use `import` instead of `require`
+    // With import loading, it is impossible to make cache invalidation
+    // See: https://github.com/nodejs/modules/issues/307
+    jsLoader: 'require',
+
+    spec_dir: config.test,
+    spec_files: [
+      path.join(config.test, 'base.spec.js'),
+      path.join(config.test, '**', '*.spec.js'),
+    ],
+
+    env: {
+      failSpecWithNoExpectations: false,
+      stopSpecOnExpectationFailure: false,
+      stopOnSpecFailure: false,
+      random: false,
+    },
+  });
+
+  jasmine.configureDefaultReporter({
+    timer: new jasmine.jasmine.Timer(),
+    showColors: true,
+    verbose: false,
+  });
+
+  return jasmine;
+}
+
+/**
+ * Require given module by removing it from the require cache before.
+ * The idea here is to be sure that a given module is always loaded on each require call,
+ * instead of being resolved from the cache.
+ *
+ * @param {string} key Module id.
+ * @returns {*} The module.
+ */
+function requireNoCache(key) {
+  delete require.cache[
+      require.resolve(key)
+  ];
+
+  return require(key);
 }
 
 module.exports = {
